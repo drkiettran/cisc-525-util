906c4852cdf1e8b10a08c401f8aef7bb1dede7e9
Kiet T. Tran, Ph.D <ktran@harrisburgu.edu>
2020-04-26 07:20:24 -0400

    Updated prepare-hadoop data to copy from ~/cisc_525/airline folder instead

--- prepare-hadoop-data.bash
+++ prepare-hadoop-data.bash
@@ -2,5 +2,5 @@
 hdfs dfs -mkdir -p /user/student/airline
 hdfs dfs -mkdir -p /user/student/shakespeare
 hdfs dfs -mkdir /tmp
-hdfs dfs -copyFromLocal ./data/airline/* /user/student/airline
+hdfs dfs -copyFromLocal ../airline/*.csv /user/student/airline
 hdfs dfs -copyFromLocal ./data/shakespeare/* /user/student/shakespeare
