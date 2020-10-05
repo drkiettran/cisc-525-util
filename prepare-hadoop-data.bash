d5594742fbb38ae552e8d25decfaedc7356690dd
Kiet T. Tran, Ph.D <ktran@harrisburgu.edu>
2020-10-05 17:07:16 -0400

    added covid data

--- prepare-hadoop-data.bash
+++ prepare-hadoop-data.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 hdfs dfs -mkdir -p /user/student
 hdfs dfs -mkdir /tmp
-hdfs dfs -copyFromLocal $HOME/cisc525/airline /user/student
-hdfs dfs -copyFromLocal $HOME/cisc525/cisc-525-util/data/shakespeare /user/student
-hdfs dfs -copyFromLocal $HOME/cisc525/COVID-19/csse_covid_19_data /user/student
+hdfs dfs -copyFromLocal $HOME/cisc_525/airline /user/student
+hdfs dfs -copyFromLocal $HOME/cisc_525/cisc-525-util/data/shakespeare /user/student
+hdfs dfs -copyFromLocal $HOME/cisc_525/COVID-19/csse_covid_19_data /user/student
